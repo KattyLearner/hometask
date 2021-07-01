6d05594ed920971c985974df236d3caf8bbf2f66
KattyLearner <ekaterina.derzhitskaya@gmail.com>
2021-07-01 14:57:44 +0300

    adding styles

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import {PATH} from "./Routes";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            <NavLink to={PATH.PRE_JUNIOR}> pre-junior </NavLink>
-            <NavLink to={PATH.JUNIOR}> junior </NavLink>
-
-
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.link} activeClassName={s.active}> pre-junior </NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.link} activeClassName={s.active}> junior </NavLink>
+            <NavLink to={PATH.JUNIOR_PlUS} className={s.link} activeClassName={s.active}> junior-plus </NavLink>
+            <div className={s.block}/>
         </div>
     )
 }
