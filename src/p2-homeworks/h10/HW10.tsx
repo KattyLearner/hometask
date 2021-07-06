d7c1ac036ac934459f84843c8ef630aaed436bdc
KattyLearner <ekaterina.derzhitskaya@gmail.com>
2021-07-06 17:50:04 +0300

    HW10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import {useDispatch, useSelector} from "react-redux";
+import {loadingAC} from "./bll/loadingReducer";
+import {AppStoreType} from "./bll/store";
+import Loader from "./bll/loader";
+import s from './Loading.module.css'
 
 function HW10() {
     // useSelector, useDispatch
-    const loading = false;
+    // const loading = false;
+    const dispatch = useDispatch()
+    const loading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+
 
     const setLoading = () => {
-        // dispatch
+        dispatch(loadingAC(true))
+
         // setTimeout
-        console.log("loading...");
+       setTimeout(()=> {
+           dispatch(loadingAC( false))
+       }, 2000)
     };
 
     return (
@@ -19,9 +30,9 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <Loader/>
                 ) : (
-                    <div>
+                    <div className={s.load}>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
                     </div>
                 )
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,23 @@
-const initState = {
+import {AppStoreType} from "./store";
 
+const initState = {
+isLoading: false
 };
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type LoadingACType = {
+    type: 'SET-LOADING'
+    isLoad: boolean
+}
+
+export const loadingReducer = (state = initState, action: LoadingACType): typeof initState => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case 'SET-LOADING': {
+            return { ...state,
+                isLoading: action.isLoad
+            };
         }
         default: return state;
     }
 };
 
-export const loadingAC = (): any => {}; // fix any
+export const loadingAC = (isLoad: boolean): LoadingACType => { return {type: 'SET-LOADING', isLoad }}; // fix any
